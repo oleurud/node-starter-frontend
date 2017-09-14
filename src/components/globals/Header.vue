fb69ab47985c5bf242482d05b2240170ab52e77c
Simon <oleurud@gmail.com>
2017-09-14 23:38:27 +0200

    fix header responsive navbar-collapse

--- src/components/globals/Header.vue
+++ src/components/globals/Header.vue
@@ -1,31 +1,33 @@
 <template>
     <div>
         <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-            <router-link class="navbar-brand" to="/">Node Starter</router-link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <div class="container">
+                <router-link class="navbar-brand" to="/">Node Starter</router-link>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavbar" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <div class="collapse navbar-collapse">
-                <ul class="navbar-nav mr-auto">
+                <div class="collapse navbar-collapse" id="mainNavbar">
+                    <ul class="navbar-nav mr-auto">
 
-                </ul>
-                <ul class="navbar-nav" v-if="user">
-                    <li class="nav-item">
-                        <router-link class="nav-link" to="/profile">{{user.username}}</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" v-on:click="logout">Logout</a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav" v-else>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="modal" data-target="#login">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="modal" data-target="#signUp">Sign up</a>
-                    </li>
-                </ul>
+                    </ul>
+                    <ul class="navbar-nav" v-if="user">
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/profile">{{user.username}}</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" v-on:click="logout">Logout</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav" v-else>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="modal" data-target="#login">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="modal" data-target="#signUp">Sign up</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
 
